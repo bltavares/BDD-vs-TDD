bfc2bab0e4622518de0a00808d7c43a24544cb13
Bruno Tavares <bruno.exz@gmail.com>
2011-09-19 23:59:30 -0300

    Sum

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -8,4 +8,8 @@ class CalculatorTest < MiniTest::Unit::TestCase
   def test_sum_positive_numbers_are_ok
     assert BddVsTdd::Calculator.new.add(1,2), 3
   end
+
+  def test_sum_of_negative_numbers_are_ok
+    assert BddVsTdd::Calculator.new.add(1, -1), 0
+  end
 end
