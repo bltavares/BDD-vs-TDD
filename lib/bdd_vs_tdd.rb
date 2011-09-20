03f375b68c8231341d62c8dcab9fb5a3edce6b3b
Bruno Tavares <bruno.exz@gmail.com>
2011-09-19 23:21:45 -0300

    Subtracting

--- lib/bdd_vs_tdd.rb
+++ lib/bdd_vs_tdd.rb
@@ -5,5 +5,9 @@ module BddVsTdd
     def add(n1, n2)
       n1 + n2
     end
+
+    def subtract(n1,n2)
+      n1 - n2
+    end
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,10 +1,20 @@
 require "spec_helper"
 
 describe BddVsTdd::Calculator do
- describe "#add" do
-  it "should sum numbers correctely" do
-    subject.add(1,2).should be == 3
+  describe "#add" do
+    it "should sum numbers correctely" do
+      subject.add(1,2).should be == 3
+    end
   end
- end
+
+  describe "#subtract" do
+    it "should subtract correctely positive numbers" do
+      subject.subtract(1,2).should be == -1
+    end
+
+    it "should subtract correctely negative numbers" do
+      subject.subtract(1, -2).should be == 3
+    end
+  end
 
 end
