24a678e65f14d1402a1e0689cfd4fc6790bed303
Bruno Tavares <bruno.exz@gmail.com>
2011-09-20 00:20:28 -0300

    Division

--- lib/bdd_vs_tdd.rb
+++ lib/bdd_vs_tdd.rb
@@ -14,5 +14,9 @@ module BddVsTdd
     def multiply(n1, n2)
       n1 * n2
     end
+
+    def divide(n1,n2)
+      n1 / (n2 != 0 ? n2.to_f : 0)
+    end
   end
 end
--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -9,27 +9,38 @@ class CalculatorTest < MiniTest::Unit::TestCase
   end
 
   def test_sum_positive_numbers_are_ok
-    assert @calculator.add(1,2), 3
+    assert_equal @calculator.add(1,2), 3
   end
 
   def test_sum_of_negative_numbers_are_ok
-    assert @calculator.add(1, -1), 0
+    assert_equal @calculator.add(1, -1), 0
   end
 
   def test_subtract_negative_numbers_are_ok
-    assert @calculator.subtract(-1,-1), 0 
+    assert_equal @calculator.subtract(-1,-1), 0 
   end
 
   def test_subtract_positive_numbers_are_ok
-    assert @calculator.subtract(1,1), 0
+    assert_equal @calculator.subtract(1,1), 0
   end
 
   def test_multiplication_of_positive_numbers_are_ok
-    assert @calculator.multiply(1, 2), 2
+    assert_equal @calculator.multiply(1, 2), 2
   end
   
   def test_multiplication_of_negative_numbers_are_ok
-    assert @calculator.multiply(-1, 2), -2
+    assert_equal @calculator.multiply(-1, 2), -2
   end
 
+  def test_divison_of_positive_numbers_are_ok
+    assert_equal @calculator.divide(1,2), 0.5
+  end
+  
+  def test_divison_of_negative_numbers_are_ok
+    assert_equal @calculator.divide(-1,2), -0.5
+  end
+
+  def test_division_by_zero_raises_exception
+    assert_raises(ZeroDivisionError) { @calculator.divide(1,0) } 
+  end
 end
