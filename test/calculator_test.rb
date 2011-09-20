509aa4c753142a8f20be8054d1a3aea359a781e6
Bruno Tavares <bruno.exz@gmail.com>
2011-09-20 00:01:16 -0300

    Refactoring

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -4,12 +4,15 @@ require "bdd_vs_tdd"
 require "turn"
 
 class CalculatorTest < MiniTest::Unit::TestCase
+  def setup
+    @calculator = BddVsTdd::Calculator.new
+  end
 
   def test_sum_positive_numbers_are_ok
-    assert BddVsTdd::Calculator.new.add(1,2), 3
+    assert @calculator.add(1,2), 3
   end
 
   def test_sum_of_negative_numbers_are_ok
-    assert BddVsTdd::Calculator.new.add(1, -1), 0
+    assert @calculator.add(1, -1), 0
   end
 end
